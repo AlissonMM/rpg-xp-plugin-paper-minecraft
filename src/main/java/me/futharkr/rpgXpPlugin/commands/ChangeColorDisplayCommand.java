e62811713ac66d0288902a3dd321956ba2ce3e21
AlissonMM <alissonmmedeji@gmail.com>
2025-10-18 14:02:23 -0300

    feat: Remove unused method call in ChangeColorDisplayCommand

--- src/main/java/me/futharkr/rpgXpPlugin/commands/ChangeColorDisplayCommand.java
+++ src/main/java/me/futharkr/rpgXpPlugin/commands/ChangeColorDisplayCommand.java
@@ -1,7 +1,6 @@
 package me.futharkr.rpgXpPlugin.commands;
 
 import me.futharkr.rpgXpPlugin.RpgXpPlugin;
-import me.futharkr.rpgXpPlugin.managers.LevelDisplayManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -25,45 +24,38 @@ public class ChangeColorDisplayCommand extends Command {
     public boolean execute(@NotNull CommandSender commandSender,
                            @NotNull String label,
                            @NotNull String[] args) {
-        try {
 
-            if (args.length < 2) {
-                commandSender.sendMessage("Correct usage: /rpgxp-changecolor changecolor <color>");
-                return true;
-            }
-            String colorName = args[1].toUpperCase();
-            ChatColor color;
-            try {
-                color = ChatColor.valueOf(colorName);
-            } catch (IllegalArgumentException e) {
-                commandSender.sendMessage("Invalid color: " + colorName + " Use valid minecraft color names.");
-                return true;
-            }
-            // Salva a configuração
-            FileConfiguration config = RpgXpPlugin.getInstance().getConfig();
-            config.set("scoreboard-color", color.name());
-            RpgXpPlugin.getInstance().saveConfig();
-            int changed = 0;
 
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                if (LevelDisplayManager.isCitizensNPC(player)) continue;
-                Scoreboard scoreboard = player.getScoreboard();
-                if (scoreboard == null) continue;
-                Objective objective = scoreboard.getObjective("playerLevel");
-                if (objective != null) {
-                    objective.setDisplayName(color + "lvl");
-                    changed++;
-                }
-            }
-
-            commandSender.sendMessage("Changed color to " + color + color.name() + ChatColor.RESET + " for " + changed + " players.");
-            return true;
-
-        } catch (Throwable t) {
-            RpgXpPlugin.getInstance().getLogger().severe("[ChangeColorDisplayCommand] error: " + t.getMessage());
-            t.printStackTrace();
-            commandSender.sendMessage("Ocorreu um erro ao executar o comando. Veja o console para detalhes.");
+        if (args.length < 2){
+            commandSender.sendMessage("Correct usage: /rpgxp-changecolor changecolor <color>");
             return true;
         }
+        String colorName = args[1].toUpperCase();
+        ChatColor color;
+        try {
+            color = ChatColor.valueOf(colorName);
+        } catch (IllegalArgumentException e) {
+            commandSender.sendMessage("Invalid color: " + colorName + " Use valid minecraft color names.");
+            return true;
+        }
+        // Saves the configuration
+        FileConfiguration config = RpgXpPlugin.getInstance().getConfig();
+        config.set("scoreboard-color", color.name());
+        RpgXpPlugin.getInstance().saveConfig();
+        int changed = 0;
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            Scoreboard scoreboard = player.getScoreboard();
+            Objective objective = scoreboard.getObjective("playerLevel");
+            if (objective != null) {
+                objective.setDisplayName(color + "lvl");
+                changed++;
+            }
+        }
+        commandSender.sendMessage("Changed color to " + color + color.name() + ChatColor.RESET + " for " + changed + " players.");
+        return true;
     }
+
+
+
+
 }
